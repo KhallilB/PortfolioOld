2448073fa09e681ebc67e6a1ff5a7b13ddc82a0a
KhallilB <khallil.bailey@gmail.com>
2019-08-30 10:56:22 -0700

    [add] basic server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
+// DEPENDENCIES
 const express = require('express');
+
+const server = express();
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+  console.log(`Server is listening on localhost: ${port}`);
+});
